7829e8c0f1446d5e0ddca51b40a3e83f1d2c93a5
moukil01 <moukil01@gmail.com>
2018-11-06 18:14:09 +0100

    training part 2

--- src/exercices/Person.java
+++ src/exercices/Person.java
@@ -2,37 +2,63 @@ package exercices;
 
 public class Person {
 
-    // TODO 1. Add the following private attributes :
-    // - name (String)
+
+
+
+
+
+
+    // TODO 1. Ajoutez les attributs privés suivants:
+    // - nom (chaîne)
     // - age (int)
-    // - wilder (boolean)
+    // - plus sauvage (booléen)
+private String name;
+private int age;
+private boolean wilder;
 
 
-    // TODO 2. Add a Constructor with name and age arguments (in this order)
-    // This constructor must initialize name and age attributes
+    // TODO 2. Ajouter un constructeur avec les arguments name et age (dans cet ordre)
+    // Ce constructeur doit initialiser les attributs name et age
 
+public Person (String name, int age){
+	this.name = name;
+	this.age = age;
+}
+    // TODO 3. Ajout de la méthode getter pour l'attribut name
+public String getName() {
+	return this.name;
+}
 
-    // TODO 3. Add getter method for name attribute
+    // TODO 4. Ajouter une méthode de définition pour l'attribut name
+public void setName(String name) {
+	this.name = name;
+}
 
+    // TODO 5. Ajouter une méthode de lecture pour l'attribut age
+public int getAge() {
+	return this.age;
+}
 
-    // TODO 4. Add setter method for name attribute
+    // TODO 6. Ajouter une méthode de définition pour l'attribut age
+public void setAge(int age) {
+	this.age = age;
+}
 
+    // TODO 7. Ajouter une méthode getter pour l'attribut wilder
+public boolean isWilder() {
+	return this.wilder;
+}
 
-    // TODO 5. Add getter method for age attribute
+    // TODO 8. Ajouter une méthode de définition pour l'attribut wilder
+public void setWilder(boolean wilder) {
+	this.wilder = wilder;
+}
 
-
-    // TODO 6. Add setter method for age attribute
-
-
-    // TODO 7. Add getter method for wilder attribute
-
-
-    // TODO 8. Add setter method for wilder attribute
-
-
-    // TODO 9. Create a method 'whoAmI' that returns "My name is {name} and I'm {age}"
-    // where you replace {name} and {age} with attributes values
-
+    // TODO 9. Créez une méthode 'whoAmI' qui renvoie "Mon nom est {name} et j'ai {age}"
+    // où vous remplacez {name} et {age} par des valeurs d'attributs
+public String whoAmI() {
+	return "My name is "  +  this.getName() + " and I'm " + this.getAge();
+}
 
 
 
